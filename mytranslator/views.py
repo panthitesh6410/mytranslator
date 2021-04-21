4f99ae501c867f6363ecea7e88a530cf5fecae63
panthitesh6410 <panthitesh6410@gmail.com>
2021-04-21 22:27:07 +0530

    added language translation functionality for multiple languages

--- mytranslator/views.py
+++ mytranslator/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import googletrans
+from googletrans import Translator
 
-def index(requests):
-    return HttpResponse("first view") 
+def index(request):
+    display_flag = False
+    d = {}
+    d.update({'display_flag': display_flag})
+    languages = googletrans.LANGUAGES
+    d.update({'languages': languages})
+    if request.method == 'POST':
+        display_flag = True
+        d.update({'display_flag': display_flag})
+        text = request.POST['text']
+        d.update({'text': text})
+        source = request.POST['source']
+        d.update({'source': source})
+        destination = request.POST['destination']
+        d.update({'destination': destination})
+        print(text, source, destination)
+        translator = Translator()
+        print("1st-------")
+        result = translator.translate(text, src=source, dest=destination)
+        print(result)
+        d.update({'result': result.text})
+    return render(request, 'mytranslator/index.html', d) 
+

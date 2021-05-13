cdb6c34ca5b26443d408fdde8a566c5e2b251a0c
panthitesh6410 <panthitesh6410@gmail.com>
2021-05-13 22:38:39 +0530

    added logic for speech to text and speech convertion

--- mytranslator/views.py
+++ mytranslator/views.py
@@ -9,7 +9,7 @@ import os
 import random
 import string
 import speech_recognition as sr
-import pyttsx3
+
 
 def index(request):
     display_flag = False
@@ -17,30 +17,68 @@ def index(request):
     d.update({'display_flag': display_flag})
     languages = googletrans.LANGUAGES
     d.update({'languages': languages})
+
     if request.method == 'POST':
-        display_flag = True
-        d.update({'display_flag': display_flag})
-        text = request.POST['text']
-        d.update({'text': text})
-        source = request.POST['source']
-        d.update({'source': source})
-        destination = request.POST['destination']
-        d.update({'destination': destination})
-        print(text, source, destination)
-        # translator ------------------------------
-        translator = Translator()
-        result = translator.translate(text, src=source, dest=destination)
-        # print(result)
-        d.update({'result': result.text})
-        #------------------------------------------
-        # speech ----------------------------------
-        try:
-            tts = gtts.gTTS(result.text)
-            random_str = ''.join(random.choices(string.ascii_uppercase+string.digits, k=10))    # generating random string -
-            tts.save("audio_files/"+str(random_str)+".mp3")
-            playsound("audio_files/"+str(random_str)+".mp3")
-        except Exception as e:
-            print("e = ", e)
-        #------------------------------------------
+
+        if "text" in request.POST:     # TEXT TO TEXT AND SPEECH CONVERTION
+            display_flag = True
+            d.update({'display_flag': display_flag})
+            text = request.POST['text']
+            d.update({'text': text})
+            source = request.POST['source']
+            d.update({'source': source})
+            destination = request.POST['destination']
+            d.update({'destination': destination})
+            print(text, source, destination)
+            # translator ------------------------------
+            translator = Translator()
+            result = translator.translate(text, src=source, dest=destination)
+            # print(result)
+            d.update({'result': result.text})
+            #------------------------------------------
+            # speech ----------------------------------
+            try:
+                tts = gtts.gTTS(result.text)
+                random_str = ''.join(random.choices(string.ascii_uppercase+string.digits, k=10))    # generating random string -
+                tts.save("audio_files/"+str(random_str)+".mp3")
+                playsound("audio_files/"+str(random_str)+".mp3")
+            except Exception as e:
+                print("e = ", e)
+            #------------------------------------------
+
+        else:         # SPEECH TO TEXT AND SPEECH CONVERTION
+            display_flag = True
+            d.update({'display_flag': display_flag})
+            source = request.POST['source']
+            d.update({'source': source})
+            destination = request.POST['destination']
+            d.update({'destination': destination})
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                print("Say Something")
+                audio = r.listen(source)
+                print("Time Over, Thanks")
+            try:
+                text = str(r.recognize_google(audio))
+                print("text - ", text)
+                d.update({'text': text})
+            except:
+                pass
+            # translator ------------------------------
+            translator = Translator()
+            result = translator.translate(text, src=source, dest=destination)
+            # print(result)
+            d.update({'result': result.text})
+            #------------------------------------------
+            # speech ----------------------------------
+            try:
+                tts = gtts.gTTS(result.text)
+                random_str = ''.join(random.choices(string.ascii_uppercase+string.digits, k=10))    # generating random string -
+                tts.save("audio_files/"+str(random_str)+".mp3")
+                playsound("audio_files/"+str(random_str)+".mp3")
+            except Exception as e:
+                print("e = ", e)
+            #------------------------------------------
+
     return render(request, 'mytranslator/index.html', d) 
 

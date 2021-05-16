5e5c68c01bffe607f0ba908b6335af4154e7bcf6
panthitesh6410 <panthitesh6410@gmail.com>
2021-05-16 19:53:30 +0530

    Created two seperate views for both speech to speech & text and text to speech & text

--- mytranslator/urls.py
+++ mytranslator/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('source_text/', views.source_text, name='source_text'),
+    path('source_speech/', views.source_speech, name='source_speech')
 ]

87a51297c137d29fd5934b49d99b214cde737659
panthitesh6410 <panthitesh6410@gmail.com>
2021-05-21 11:39:03 +0530

    static bug resolving

--- translator/settings.py
+++ translator/settings.py
@@ -127,9 +127,6 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR, 'static'),
-)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
